9c41fab266fe4c0ccb3b629669ca826176ef2c47
EduardoVP-0 <eduardo.velazquez71@unach.mx>
2025-07-17 10:24:58 -0600

    Agregar archivos de proyecto.

---
+++ pruebatmp/WFrm_Incidencias.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="WFrm_Incidencias.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Probar conexión a SQL Server con botón</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnProbarConexion" runat="server" Text="Probar conexión" OnClick="btnProbarConexion_Click" />
+            <br /><br />
+            <asp:Label ID="lblResultado" runat="server" Text=""></asp:Label>
+        </div>
+        <div>
+            <asp:Button ID="btnMostrarVista" runat="server" Text="Mostrar Vista" OnClick="btnMostrarVista_Click" />
+            <br /><br />
+            <asp:GridView ID="gvVista" runat="server" AutoGenerateColumns="True"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ pruebatmp/WFrm_Incidencias.aspx.vb
@@ -0,0 +1,55 @@
+﻿Imports System.Data.SqlClient
+Imports System.Configuration
+Imports System.Data
+
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub btnProbarConexion_Click(ByVal sender As Object, ByVal e As EventArgs)
+        ' Cadena de conexión: cámbiala con tus datos
+        Dim connectionString As String = "Server=172.16.39.64;Database=SimadOC;User Id=Sa;Password=Semuigen2025;"
+
+
+
+        Dim connection As New SqlConnection(connectionString)
+
+        Try
+            connection.Open()
+            lblResultado.Text = "✅ Conexión exitosa a la base de datos."
+        Catch ex As Exception
+            lblResultado.Text = "❌ Error de conexión: " & ex.Message
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+    Protected Sub btnMostrarVista_Click(ByVal sender As Object, ByVal e As EventArgs)
+        ' Cadena de conexión (puedes usar la del web.config)
+        Dim connectionString As String = "Server=172.16.39.64;Database=SimadOC;User Id=Sa;Password=Semuigen2025;"
+        ' O:
+        ' Dim connectionString As String = ConfigurationManager.ConnectionStrings("MiConexionSQL").ConnectionString
+
+        ' Tu consulta SELECT de la vista
+        Dim query As String = "SELECT * FROM VwRpt_Incidencias"
+
+        Using connection As New SqlConnection(connectionString)
+            Dim cmd As New SqlCommand(query, connection)
+            Dim adapter As New SqlDataAdapter(cmd)
+            Dim dt As New DataTable()
+
+            Try
+                connection.Open()
+                adapter.Fill(dt)
+
+                gvVista.DataSource = dt
+                gvVista.DataBind()
+
+            Catch ex As Exception
+                ' Maneja el error
+                ' Puedes usar un Label para mostrar el error si quieres
+            Finally
+                connection.Close()
+            End Try
+        End Using
+    End Sub
+
+End Class
